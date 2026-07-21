cde0b1fe60c420b36ea7db4616107ef37d154c39
kalmatovski <jnshbkvkbr@gmail.com>
2026-07-21 10:52:35 +0600

    implementing FastApi

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from app.matching import create_match_result
+from app.schemas import MatchRequest, MatchResponse
+
+app = FastAPI()
+
+app.post("/match", response_model=MatchResponse)
+def match_resume(request:MatchRequest) -> MatchResponse:
+    result = create_match_result(resume=request.resume_text,
+                                 job=request.job_description)
+    
+    return MatchResponse(**result)
---
+++ app/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class MatchRequest(BaseModel):
+    resume_text: str
+    job_description: str
+
+
+class MatchResponse(BaseModel):
+    match_score: float
+    matched_skills: list[str]
+    missing_skills: list[str]
+
+
+
+
